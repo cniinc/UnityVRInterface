db9a4dcc21b9391ce048f44210265221d6d8632c
Jason Mathias <software@positrongames.com>
2016-04-07 16:45:51 -0700

    Making a rotator panel - bug with object zeroing, and needs to be built around (0,0,0) for now

---
+++ Assets/_Scripts/RotatingPanel.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+//[ExecuteInEditMode]
+public class RotatingPanel : MonoBehaviour {
+
+	public Camera ViewersCamera;
+	public float MeterDistanceFromCamera;
+	public float TotalWrapAroundRangeInDegrees;
+	public GameObject[] ObjectsInOrder;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyUp (KeyCode.F))
+			placeObjectsInOrder ();
+	
+	}
+
+	void placeObjectsInOrder()
+	{
+		//place the objects at MeterDistance from camera
+		foreach (GameObject go in ObjectsInOrder) 
+		{
+			go.transform.parent = this.transform;
+			transform.localPosition = Vector3.zero;
+
+		}
+
+		//place objects on a 180' spectrum
+		for(int i =0; i< ObjectsInOrder.Length; i++)
+		{
+			transform.localPosition = Vector3.left * MeterDistanceFromCamera;
+			ObjectsInOrder[i].transform.RotateAround(Vector3.zero, Vector3.up, 180 * (i + 1) / (ObjectsInOrder.Length+1));
+
+		}
+	}
+}
