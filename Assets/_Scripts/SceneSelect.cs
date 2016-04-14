4179d8f45dcd3549756046cd056d6ea1aa821c84
Jason M <software@positrongames.com>
2016-04-14 15:11:15 -0700

    Added Scene Select Function

---
+++ Assets/_Scripts/SceneSelect.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneSelect : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void LoadSceneCalled(string SceneName)
+	{
+		SceneManager.LoadScene (SceneName);
+	}
+}
