c282de043b3822899427bd407646100fea251067
Jason M <software@positrongames.com>
2016-09-29 16:34:15 -0700

    Do we need VR Assets folder?

---
+++ Assets/_Scripts/Background/GameSchoolVRManagerEditor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+[CustomEditor(typeof(GameSchoolVRManager))]
+public class GameSchoolVRManagerEditor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+}
