e944b3885ef9c828fc18522b8b6068c2ee660369
Jason M <software@positrongames.com>
2016-09-30 18:50:51 -0700

    tEMP

--- Assets/_Scripts/Background/CameraUtilities.cs
+++ Assets/_Scripts/Background/CameraUtilities.cs
@@ -3,17 +3,26 @@ using System.Collections;
 using UnityEngine.VR;
 
 public class CameraUtilities : MonoBehaviour {
-
+	bool firstUpdateFrame = true;
 	/*
 	 * a series of simple functions to streamline functions.
 	 */
 
 	// Use this for initialization
-	void Start () {
-		if (VRDevice.isPresent) 
-		{
-			GetComponent<SimpleSmoothMouseLook> ().enabled = false;
-			Debug.Log ("Turning off SimpleSmoothMouseLook because " + VRDevice.model + " detected");
+	void Update () {
+
+		if (firstUpdateFrame) {
+			firstUpdateFrame = false;
+			if (VRDevice.isPresent) {
+				GetComponent<SimpleSmoothMouseLook> ().enabled = false;
+				Debug.Log ("Turning off SimpleSmoothMouseLook because " + VRDevice.model + " detected");
+			} else {
+				
+				GetComponent<GvrHead> ().trackRotation = false;
+				GetComponentInChildren<GvrViewer> ().VRModeEnabled = false;
+			}
+
+
 		}
 	
 	}
